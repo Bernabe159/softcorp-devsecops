74c7c4bc9ba71b6285065a03025b7bb04c099cb4
Bryan <bryanbernabetorrez159@gmail.com>
2026-07-06 21:46:40 -0400

    Estructura inicial del proyecto SoftCorp DevSecOps

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # En una app real, esta credencial NUNCA debe estar en texto plano.
 # Este es un ejemplo intencional para que Gitleaks / Secret Scanning lo detecte
 # durante la demo. En la versión "corregida" se reemplaza por Vault (ver vault_client.py)
-DB_PASSWORD_INSEGURO = "Sc_AgroDB_2026_Pr0dSecret!x9Qz"  # <-- esto debe ser detectado y bloqueado
+DB_PASSWORD_INSEGURO = "AgroTech2026ProdDBPass8834xzKq"  # <-- esto debe ser detectado y bloqueado
 
 @app.route("/")
 def home():
